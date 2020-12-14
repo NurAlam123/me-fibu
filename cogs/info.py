d9d36d86ea52fdd23716e9955c4341e5926fd0c5
fibu-ph <75558172+fibu-ph@users.noreply.github.com>
2020-12-14 23:51:11 +0600

    Update info.py

--- cogs/info.py
+++ cogs/info.py
@@ -56,8 +56,7 @@ class Info(commands.Cog):
 							timestamp=time.now(), 
 							description=f"```{guild}```")
 				msg.set_thumbnail(url=f"{ctx.guild.icon_url}")
-				region = guild.region
-				msg.add_field(name="Region",value=f"{region.title()}")
+				msg.add_field(name="Region",value=f"{guild.region.capitalize()}")
 				msg.add_field(name="Owner",value=owner)
 				msg.add_field(name="Members",value=f"{guild.member_count}")
 				msg.add_field(name="Bots",value=bots)
