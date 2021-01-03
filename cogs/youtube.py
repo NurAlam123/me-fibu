282a708e3192f8f80185f9e63457a4b37faafef0
fibu-ph <75558172+fibu-ph@users.noreply.github.com>
2021-01-03 20:00:24 +0530

    Added youtube.py cog
    added youtube cog

---
+++ cogs/youtube.py
@@ -0,0 +1,38 @@
+import discord
+from datetime import datetime as time
+from discord.ext import commands as c
+import wikipedia as wiki
+import covid19_data
+import urllib.parse,urllib.request,re
+import translators as trans
+import requests,json
+import asyncio, typing
+
+class Youtube(c.Cog):
+	def __init__(self,client):
+		self.client = client
+    
+#youtube
+	@c.group(aliases=["youtube","utube"],case_insensitive=True)
+	async def yt(slef,ctx):
+		if ctx.invoked_subcommand is None:
+			pass
+	@yt.command()
+	async def search(self,ctx,*,query):
+		query = query.split()
+		search_word = "+".join(query)
+		html = urllib.request.urlopen("https://www.youtube.com/results?search_query="+search_word)
+		results = re.findall("watch\?v=(\S{11})",html.read().decode())
+		try:
+			await ctx.message.add_reaction("📺")
+			await ctx.send("https://youtube.com/watch?v="+results[0])
+		except:
+			await ctx.message.add_reaction("❌")
+			msg = discord.Embed(title="Error", description="Oops.. Not found the video..\nPlease search again by typing ```!fibu yt search search_word ```")
+			await ctx.send(embed=msg)
+
+
+		
+		
+def setup(bot):
+	bot.add_cog(Youtube(bot))
