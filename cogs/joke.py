48d3bf13535766115b40c94ad2fd9fb1756d13a7
fibu-ph <75558172+fibu-ph@users.noreply.github.com>
2021-01-04 10:01:36 +0530

    Added Jokes

---
+++ cogs/joke.py
@@ -0,0 +1,23 @@
+import discord
+from datetime import datetime as time
+from discord.ext import commands as c
+import pip
+try:
+	import pyjokes as j
+except:
+	pip.main(["install","pyjokes"])
+import asyncio, typing, random
+
+class Joke(c.Cog):
+	def __init__(self,client):
+		self.client = client
+    
+    
+ #joke
+	@c.command()
+	async def joke(self,ctx):
+		joke = pyjokes.get_joke()
+    await ctx.send(joke)
+		
+def setup(bot):
+	bot.add_cog(Joke(bot))
