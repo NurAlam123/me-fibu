24a21510dcee32d9cc441a56bb9ad272cfd675db
fibu-ph <fibuph@gamil.com>
2021-03-01 19:31:48 +0600

    Update echo command

--- cogs/commands.py
+++ cogs/commands.py
@@ -2,38 +2,42 @@ import discord
 from datetime import datetime as time
 from discord.ext import commands as c
 import wikiquote as q
-import asyncio, typing, random
+import asyncio
+import typing
+import random
 
 class Command(c.Cog):
-	def __init__(self,client):
-		self.client = client
+    def __init__(self,client):
+        self.client = client
 
 #echo
-	@c.command()
-	async def echo(self,ctx, channel: typing.Optional[discord.TextChannel]=None,*,msg):
-		if channel==None:
-			await ctx.message.delete()
-			await ctx.send(msg)
-		else:
-			await ctx.message.delete()
-			await channel.send(msg)
+    @c.command()
+    async def echo(self,ctx, channel: typing.Optional[discord.TextChannel]=None,*,msg):
+        await ctx.message.delete()
+        if channel == None:
+            await ctx.send(msg)
+        else:
+            await channel.send(msg)
+        log_format = f"==========\nUser: {ctx.author}\nName: {ctx.author.name}\nID: {ctx.author.id}\nServer: {ctx.message.guild.name}\nMessage: {ctx.message.content}\n=========="
+        log_channel = await self.client.fetch_channel(802766376719876107)
+        await log_channel.send(log_format)
 
 #quotes
-	@c.command()
-	async def quote(self, ctx,*,arg=None):
-		if arg==None:
-			random_q = q.random_titles(max_titles=1)[0]
-			quote_text= random.choice(q.quotes(random_q))
-			await ctx.send(f">>> {quote_text}\n	- *{random_q}*")
-		else:
-			try:
-				search = q.search(arg)[0]
-				quote = random.choice(q.quotes(search))
-				await ctx.send(f">>> {quote}\n	- *{search}*")
-			except:
-				pass
-		
-		
-		
+    @c.command()
+    async def quote(self, ctx,*,arg = None):
+        if arg == None:
+            random_q = q.random_titles(max_titles = 1)[0]
+            quote_text = random.choice(q.quotes(random_q))
+            await ctx.send(f">>> {quote_text}\n	- *{random_q}*")
+        else:
+            try:
+                search = q.search(arg)[0]
+                quote = random.choice(q.quotes(search))
+                await ctx.send(f">>> {quote}\n	- *{search}*")
+            except:
+                pass
+
+
+
 def setup(bot):
-	bot.add_cog(Command(bot))
+    bot.add_cog(Command(bot))
