cf4b780992c9e69f09fcff9e4633a6932be84aed
nurAlam123 <nuralam.rsc@gmail.com>
2021-05-13 10:34:40 +0600

    added nqn cant get rid of unknow emoji

---
+++ cogs/nqn.py
@@ -0,0 +1,39 @@
+from discord.ext import commands
+from discord import utils
+import discord
+import re
+
+class emoji(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener("on_message")
+    async def _message(self, message):
+        if message.author.bot:
+            return
+        if ":" in message.content:
+
+            emotes = re.findall(r":\w+:", message.content)
+            user_message = message.content
+            
+            em = False
+            for emote in emotes:
+                emoji = utils.get(self.bot.emojis, name= emote.strip(":"))
+                if emoji:
+                    if emoji.animated:
+                        add = "a"
+                    else:
+                        add = ""
+                    server_emoji = f"<{add}:{emoji.name}:{emoji.id}>"
+                    user_message = user_message.replace(emote, server_emoji)
+                    em = True
+            if em:
+                webhooks = await message.channel.webhooks()
+                webhook = utils.get(webhooks, name = "Server Emoji")
+                if webhook is None:
+                    webhook = await message.channel.create_webhook(name = "Server Emoji")
+                await webhook.send(user_message, username = message.author.name, avatar_url = message.author.avatar_url)
+                await message.delete()
+
+def setup(bot):
+	bot.add_cog(emoji(bot))
