6422e46ccdc7aa37cce1c07e861be3a3445b342b
nurAlam123 <nuralam.rsc@gmail.com>
2021-05-26 14:14:59 +0600

    version 0.2.6

--- cogs/nqn.py
+++ cogs/nqn.py
@@ -26,10 +26,6 @@ class emoji(commands.Cog):
                         server_emoji = f"<a:{emoji.name}:{emoji.id}>"
                         user_message = user_message.replace(emote, server_emoji)
                         em = True
-                    if not emoji.animated and emoji not in message.guild.emojis:
-                        em = True
-                        get_emoji = f"<:{emoji.name}:{emoji.id}>"
-                        user_message = user_message.replace(emote, get_emoji)
                         
             if em:
                 webhooks = await message.channel.webhooks()
@@ -38,7 +34,7 @@ class emoji(commands.Cog):
                     webhook = await message.channel.create_webhook(name = "Server Emoji")
                 if message.reference:
                     get_message = await message.channel.fetch_message(message.reference.message_id)
-                    em = discord.Embed(title= f"{message.author.mention} replied {get_message.author.mention}'s message", description= f"[Jump to that message!]({get_message.jump_url})")
+                    em = discord.Embed(title= f"{message.author} replied {get_message.author}'s message", description= f"[Jump to that message!]({get_message.jump_url})")
                     em.set_author(name= get_message.author.name, icon_url= get_message.author.avatar_url)
                     await webhook.send(content= user_message, embed= em, username= message.author.name, avatar_url= message.author.avatar_url)
                 else:
