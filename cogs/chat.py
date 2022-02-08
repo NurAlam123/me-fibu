d9cf4d753e85acaa3fc131ee3d44173bdba45d67
Nur Alam <nuralam.rsc@gmail.com>
2022-02-08 13:10:41 +0600

    chat command done

--- cogs/chat.py
+++ cogs/chat.py
@@ -48,33 +48,30 @@ class Chat(commands.Cog):
 
     @commands.command(name="chat_channel")
     @commands.has_permissions(administrator=True)
-    async def chat_channel(self, ctx, channel: discord.TextChannel = None):
-        if channel:
-            db, tb = Chat.database()
-            info = tb.find_one({"name": "chat_info"})
-            chat_channel = info.get("channel")
-            if chat_channel:
-                await ctx.send("Do you really want to change the chat channel? (y/n)")
+    async def chat_channel(self, ctx, channel: discord.TextChannel):
+        db, tb = Chat.database()
+        info = tb.find_one({"name": "chat_info"})
+        chat_channel = info.get("channel")
+        if chat_channel:
+            await ctx.send("Do you really want to change the chat channel? (y/n)")
 
-                def check(m):
-                    return m.author == ctx.author and m.channel == ctx.channel
-                try:
-                    msg = await self.bot.wait_for('message', check=check, timeout=60)
-                except asyncio.TimeoutError:
-                    await ctx.send("Timeout!!\nTry again.")
-                else:
-                    if msg.content.lower() == "y":
-                        tb.update_one({"name": "chat_info"}, {
-                                      "$set": {"channel": channel.id}})
-                        await ctx.send(f"Chat channel changed to: {channel.mention}")
-                    else:
-                        await ctx.send("Canceled.")
+            def check(m):
+                return m.author == ctx.author and m.channel == ctx.channel
+            try:
+                msg = await self.bot.wait_for('message', check=check, timeout=60)
+            except asyncio.TimeoutError:
+                await ctx.send("Timeout!!\nTry again.")
             else:
-                tb.update_one({"name": "chat_info"}, {
-                              "$set": {"channel": channel.id}})
-                await ctx.send(f"Chat channel set to: {channel.mention}")
+                if msg.content.lower() == "y":
+                    tb.update_one({"name": "chat_info"}, {
+                                    "$set": {"channel": channel.id}})
+                    await ctx.send(f"Chat channel changed to: {channel.mention}")
+                else:
+                    await ctx.send("Canceled.")
         else:
-            await ctx.send("Mention a channel or paste the id")
+            tb.update_one({"name": "chat_info"}, {
+                            "$set": {"channel": channel.id}})
+            await ctx.send(f"Chat channel set to: {channel.mention}")
 
     @commands.command(name="chat")
     async def chat(self, ctx, *, message):
@@ -92,35 +89,33 @@ class Chat(commands.Cog):
                 if ref_message:
                     ref_message_id = ref_message.message_id
                     ref_message_obj = await ctx.channel.fetch_message(ref_message_id)
-                    ref_message_content = ref_message_obj.content.lstrip(
-                        "!fibu chat ")
+                    ref_message_content = ref_message_obj.content.replace("!fibu chat ", "")
                     chat_channel = ctx.guild.get_channel(chat_channel_id)
-                    channel_messages = await chat_channel.history(limit=20).flatten()
+                    channel_messages = await chat_channel.history(limit=100).flatten()
                     for channel_message in channel_messages:
+                        print(channel_message.content, ref_message_content)
                         if channel_message.content == ref_message_content:
                             await channel_message.reply(message)
                             break
                 else:
                     chat_channel = ctx.guild.get_channel(chat_channel_id)
                     await chat_channel.send(message)
-
+    
+    ###### Error handling ######
+    @chat.error
+    async def _chat_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send(f"Message is missing!!\n```\n!fibu chat <message>\n```")
+    @chat_account.error
+    async def _chat_account_error(self, ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send(f"You don't have the permissions to do that!!")
+    @chat_channel.error
+    async def _chat_channel_error(self, ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send(f"You don't have the permissions to do that!!")
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send(f"Channel is missing!!\n```\n!fibu chat_channel <channel>\n```")
 
 def setup(bot):
     bot.add_cog(Chat(bot))
-
-
-# if __name__ == "__main__":
-#     con_fibu = pymongo.MongoClient(
-#         "mongodb+srv://fibu-ph:FibuProgrammingHero@fibu.vtsjw.mongodb.net/fibu?retryWrites=true&w=majority")
-#     # db
-#     # con_fibu = pymongo.MongoClient(os.getenv("DB"))
-#     db = con_fibu["fibu"]  # database
-#     tb = db["other_data"]  # table
-#     # accounts = tb.find_one({"name": "chat_info"})
-
-#     # tb.update_one({"name": "chat_info"}, {"$set": {"fibu": 5467}})
-#     tb.insert_one({"name": "chat_info"})
-#     # print(accounts.get("fibu"))
-#     # tb.delete_one({"name": "chat_account"})
-#     for i in tb.find():
-#         print(i)
