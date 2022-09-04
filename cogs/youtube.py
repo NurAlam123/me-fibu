d4dd7f4a6b309d0e904fc966d6df7b31369c1187
Nur Alam <nuralam.rsc@gamil.com>
2022-09-04 22:17:55 +0600

    channel add and video check done

--- cogs/youtube.py
+++ cogs/youtube.py
@@ -1,26 +1,51 @@
 import discord
 from datetime import datetime as time
-from discord.ext import commands
+from discord.ext import commands, tasks
+from discord.ext.commands import has_permissions
 from pyyoutube import Api
 import os
 import asyncio
+import pymongo
+from pytube import Channel
 
 API_KEY = os.getenv("YT_API")
 
 yt_api = Api(api_key=API_KEY)
 
-class Youtube(commands.Cog):
-    def __init__(self,client):
-        self.bot = client
 
-## YouTube video search
-    @commands.group(aliases=["youtube","utube"],case_insensitive=True)
-    async def yt(self,ctx):
+class Youtube(commands.Cog):
+
+    con_fibu = pymongo.MongoClient(os.getenv("DB"))
+    db = con_fibu["fibu"]  # database
+    tb = db["other_data"]
+
+    def __init__(self, client):
+        self.bot = client
+        self.db()
+
+        # self.yt_ch = "https://www.youtube.com/channel/UCfzA2NPW1UUm-Q6kpVkbN5w"
+        # self.last_video_id = None
+        self.check_video.start()
+
+    # database
+    def db(self):
+        yt_db = Youtube.tb.find_one({"name": "youtube_notify"})
+        data = yt_db.get("data")
+        if not data:
+            self.ytData = {}
+        else:
+            self.ytData = data
+
+    # commands
+    @commands.group(aliases=["youtube", "utube"], case_insensitive=True)
+    async def yt(self, ctx):
         if ctx.invoked_subcommand is None:
             pass
+
     @yt.command(aliases=["vdo", "v"])
     async def video(self, ctx, *, query):
-        yt_video = yt_api.search_by_keywords(q = query, safe_search = "strict", search_type = "video")
+        yt_video = yt_api.search_by_keywords(
+            q=query, safe_search="strict", search_type="video")
         if yt_video.items != []:
             limit = 10
             yt_url = "https://youtube.com/watch?v="
@@ -33,10 +58,10 @@ class Youtube(commands.Cog):
             url_msg = await ctx.send(video_urls[0])
             page = 1
             pages = limit
-            
+
             emojis = ["\N{Black Left-Pointing Triangle}",
-                              "\N{Black Right-Pointing Triangle}"]
-            
+                      "\N{Black Right-Pointing Triangle}"]
+
             last_page = False
             reverse = False
             out_emoji = False
@@ -55,7 +80,7 @@ class Youtube(commands.Cog):
                     await url_msg.add_reaction(emojis[0])
                 else:
                     if not reverse:
-                        if (page-1)<=1 or last_page:
+                        if (page-1) <= 1 or last_page:
                             await url_msg.clear_reactions()
                             for emoji in emojis:
                                 await url_msg.add_reaction(emoji)
@@ -67,22 +92,21 @@ class Youtube(commands.Cog):
                     if last_page:
                         last_page = False
 
-
                 try:
-                    reaction, user = await self.bot.wait_for("reaction_add", check = reaction_check, timeout=60)
+                    reaction, user = await self.bot.wait_for("reaction_add", check=reaction_check, timeout=60)
                 except asyncio.TimeoutError:
-                        await url_msg.clear_reactions()
-                        break
-            				
-                if reaction.emoji == emojis[1] and page!=pages:
+                    await url_msg.clear_reactions()
+                    break
+
+                if reaction.emoji == emojis[1] and page != pages:
                     page += 1
-                    await url_msg.edit(content = video_urls[page-1])
+                    await url_msg.edit(content=video_urls[page-1])
                     await url_msg.remove_reaction(reaction, user)
                     reverse = False
                     out_emoji = False
                 elif reaction.emoji == emojis[0] and page > 1:
                     page -= 1
-                    await url_msg.edit(content = video_urls[page-1])
+                    await url_msg.edit(content=video_urls[page-1])
                     await url_msg.remove_reaction(reaction, user)
                     reverse = True
                     out_emoji = False
@@ -93,25 +117,28 @@ class Youtube(commands.Cog):
                     await url_msg.remove_reaction(reaction, user)
         else:
             await ctx.message.add_reaction("<:wrong:846424916404207636>")
-            msg = discord.Embed(title=":warning: Error :warning:", description="Oops.. Not found any video..\nPlease search again by typing ```!fibu yt search <video name>```")
+            msg = discord.Embed(title=":warning: Error :warning:",
+                                description="Oops.. Not found any video..\nPlease search again by typing ```!fibu yt search <video name>```")
             await ctx.send(embed=msg)
 
-## YouTube channel search
+# YouTube channel search
     @yt.command(aliases=["chnl", "c"])
-    async def channel(self,ctx,*,query):
-        yt_channel = yt_api.search_by_keywords(q = query, safe_search = "strict", search_type = "channel")
+    async def channel(self, ctx, *, query):
+        yt_channel = yt_api.search_by_keywords(
+            q=query, safe_search="strict", search_type="channel")
         if yt_channel.items != []:
             limit = 10
             yt_url = "https://youtube.com/channel/"
-            channel_urls = [yt_url+yt_channel.items[i].to_dict()["id"]["channelId"] for i in range(limit)]
+            channel_urls = [yt_url+yt_channel.items[i].to_dict()["id"]["channelId"]
+                            for i in range(limit)]
             await ctx.message.add_reaction("\N{White Heavy Check Mark}")
             url_msg = await ctx.send(channel_urls[0])
             page = 1
             pages = limit
-           
-            emojis = ["\N{Black Left-Pointing Triangle}", 
-                              "\N{Black Right-Pointing Triangle}"]
-            
+
+            emojis = ["\N{Black Left-Pointing Triangle}",
+                      "\N{Black Right-Pointing Triangle}"]
+
             last_page = False
             reverse = False
             out_emoji = False
@@ -130,7 +157,7 @@ class Youtube(commands.Cog):
                     await url_msg.add_reaction(emojis[0])
                 else:
                     if not reverse:
-                        if (page-1)<=1 or last_page:
+                        if (page-1) <= 1 or last_page:
                             await url_msg.clear_reactions()
                             for emoji in emojis:
                                 await url_msg.add_reaction(emoji)
@@ -142,23 +169,22 @@ class Youtube(commands.Cog):
                     if last_page:
                         last_page = False
 
-
                 try:
-                    reaction, user = await self.bot.wait_for("reaction_add", check = reaction_check, timeout=60)
+                    reaction, user = await self.bot.wait_for("reaction_add", check=reaction_check, timeout=60)
                 except asyncio.TimeoutError:
-                        await url_msg.clear_reactions()
-                        break
-            				
-                if reaction.emoji == emojis[1] and page!=pages:
-                    page =+ 1
-                    await url_msg.edit(content = channel_urls[page-1])
+                    await url_msg.clear_reactions()
+                    break
+
+                if reaction.emoji == emojis[1] and page != pages:
+                    page = + 1
+                    await url_msg.edit(content=channel_urls[page-1])
                     await url_msg.remove_reaction(reaction, user)
-                    #page += 1
+                    # page += 1
                     reverse = False
                     out_emoji = False
                 elif reaction.emoji == emojis[0] and page > 1:
-                    page -=1
-                    await url_msg.edit(content = channel_urls[page-1])
+                    page -= 1
+                    await url_msg.edit(content=channel_urls[page-1])
                     await url_msg.remove_reaction(reaction, user)
                     reverse = True
                     out_emoji = False
@@ -169,10 +195,93 @@ class Youtube(commands.Cog):
                     await url_msg.remove_reaction(reaction, user)
         else:
             await ctx.message.add_reaction("<:wrong:846424916404207636>")
-            msg = discord.Embed(title= ":warning: Error :warning:", description= "Oops.. Not found the channel..\nPlease search again by typing ```!fibu yt channel <channel name>```")
-            await ctx.send(embed= msg)
+            msg = discord.Embed(title=":warning: Error :warning:",
+                                description="Oops.. Not found the channel..\nPlease search again by typing ```!fibu yt channel <channel name>```")
+            await ctx.send(embed=msg)
+# YouTube channel notification
 
+    @yt.group(aliases=["alert"], case_insensitive=True)
+    async def notification(self, ctx):
+        if ctx.invoked_subcommand is None:
+            pass
 
+    @notification.command()
+    @has_permissions(administrator=True, manage_roles=True, manage_messages=True)
+    async def add(self, ctx, channel_link: str = None, post_channel: discord.TextChannel = None, *, text: str = ""):
+
+        # arguments error => channel_link and post_channel are require
+        if not channel_link:
+            await ctx.send("Provide the youtube channel link!")
+        elif not post_channel:
+            await ctx.send("Mention the discord channel where you want to get the notification message!")
+        else:
+            # get recent video
+            channel_videos = Channel(channel_link).url_generator()
+            try:
+                first_video_id = str(
+                    channel_videos.__next__()).split("?v=")[-1]
+            except:
+                first_video_id = None
+
+            dataFormat = {
+                "ytChannel": channel_link,
+                "postChannel": str(post_channel.id),
+                "textFormat": text,
+                "lastVideoID": first_video_id
+            }
+
+            get_guild_data = self.ytData.get(f"{ctx.guild.id}")
+
+            if not get_guild_data:
+                self.ytData[f"{ctx.guild.id}"] = [dataFormat]
+                self.tb.update_one({"name": "youtube_notify"}, {
+                                   "$set": {"data": self.ytData}})
+
+            else:
+                self.ytData[f"{ctx.guild.id}"].append(dataFormat)
+                self.tb.update_one({"name": "youtube_notify"}, {
+                                   "$set": {"data": self.ytData}})
+
+    async def send_message(self, ytLink: str, guildId: int, channelId: int, text: str):
+        # guild = await self.bot.fetch_channel(guildId)
+        channel = await self.bot.fetch_channel(channelId)
+        await channel.send(f"{text}\n{ytLink}")
+
+    @tasks.loop(seconds=30.0, count=10)
+    async def check_video(self):
+        print("Checking")
+        j = 0
+        guilds_data = self.ytData
+        for guild_id in guilds_data:
+            guild_data = guilds_data[guild_id]
+            i = 0
+            for data in guild_data:
+                yt_channel = data.get("ytChannel")
+                lastVideoID = data.get("lastVideoID")
+                postChannel = data.get("postChannel")
+                text = data.get("text")
+                # get all videos of that channel
+                channel_videos = Channel(yt_channel).url_generator()
+                # get first videos of that channel and store video id
+                try:
+                    first_video_id = str(
+                        channel_videos.__next__()).split("?v=")[-1]
+                except:
+                    first_video_id = None
+
+                if not lastVideoID:
+                    self.ytData[guild_id][i]["lastVideoID"] = first_video_id
+                    self.tb.update_one({"name": "youtube_notify"}, {
+                        "$set": {"data": self.ytData}})
+
+                if (lastVideoID != first_video_id) and first_video_id:
+                    ytLink = f"https://youtube.com/watch?v={first_video_id}"
+                    await self.send_message(ytLink, guild_id, postChannel, text)
+
+                    self.ytData[guild_id][i]["lastVideoID"] = first_video_id
+                    self.tb.update_one({"name": "youtube_notify"}, {
+                        "$set": {"data": self.ytData}})
+                i += 1
 
 
 def setup(bot):
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from discord.utils import get
-import discord_components as d_c
+# import discord_components as d_c
 
 import time
 from datetime import datetime
@@ -10,31 +10,35 @@ import asyncio
 import pymongo
 import logging
 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 #### logging [recommended]####
-logging.basicConfig(level= logging.INFO)
+logging.basicConfig(level=logging.INFO)
 #############################
 
 token = os.getenv("TOKEN")
 
-prefix_file = open("prefix.txt","r")
-prefixes = [i.replace("\n"," ") for i in prefix_file.readlines()]
+prefix_file = open("prefix.txt", "r")
+prefixes = [i.replace("\n", " ") for i in prefix_file.readlines()]
 
 intents = discord.Intents.all()
 intents.members = True
 
-bot = commands.Bot(command_prefix= prefixes, intents= intents, case_insensitive= True)
+bot = commands.Bot(command_prefix=prefixes,
+                   intents=intents, case_insensitive=True)
 bot.remove_command("help")
 
-d_c.DiscordComponents(bot)
+# d_c.DiscordComponents(bot)
 
 bot.TEAM = [
-                            838836138537648149, # Nur
-                            664550550527803405, # Tamim
-                            728260210464129075, # Rishikesh
-                            693375549686415381, # Soren
-                            555452986885668886 # Karim
-                        ] # our team's discord ids
+    838836138537648149,  # Nur
+    664550550527803405,  # Tamim
+    728260210464129075,  # Rishikesh
+    693375549686415381,  # Soren
+    555452986885668886  # Karim
+]  # our team's discord ids
 bot.version = 'v0.4.0'
 
 
@@ -42,28 +46,32 @@ bot.version = 'v0.4.0'
 async def on_ready():
     await bot.change_presence(activity=discord.Game(name="!fibu help | Fibu | Programming Hero"))
     print(f"Logged in as {bot.user}")
-    
-    
-    
+
+
 # ping
 @bot.command()
 async def ping(ctx):
-    msg = discord.Embed(title="Pong 🏓", description=f"{round(bot.latency*1000)} _ms_!",color=0xffdf08)
+    msg = discord.Embed(
+        title="Pong 🏓", description=f"{round(bot.latency*1000)} _ms_!", color=0xffdf08)
     await ctx.send(embed=msg)
 
 # fibu
+
+
 @bot.listen()
 async def on_message(message):
     if message.content.lower().strip() == "!fibu":
         await message.channel.send(f"Type `!fibu help` to get help message!!")
 
 # cogs load and unload
+
+
 @bot.command()
-async def on(ctx,file):
+async def on(ctx, file):
     if ctx.author.id in bot.TEAM:
         try:
             for files in os.listdir("./cogs"):
-                if files==file.lower()+".py":
+                if files == file.lower()+".py":
                     bot.load_extension(f"cogs.{files[:-3]}")
                     await ctx.send(f"{file} loaded!")
         except:
@@ -71,23 +79,22 @@ async def on(ctx,file):
     else:
         await ctx.send(f"You don't have the permission to do that!!")
 
+
 @bot.command()
-async def off(ctx,file):
-        if ctx.author.id in bot.TEAM:
-            try:
-                bot.unload_extension(f"cogs.{file.lower()}")
-                await ctx.send(f"{file} unloaded!")
-            except:
-                await ctx.send(f"{file} is already unloaded!")
-        else:
-            await ctx.send("You don't have the permission to do that!!")
-            
-            
-            
+async def off(ctx, file):
+    if ctx.author.id in bot.TEAM:
+        try:
+            bot.unload_extension(f"cogs.{file.lower()}")
+            await ctx.send(f"{file} unloaded!")
+        except:
+            await ctx.send(f"{file} is already unloaded!")
+    else:
+        await ctx.send("You don't have the permission to do that!!")
 
-for files in os.listdir("./cogs"):
-    if files.endswith(".py"):
-        bot.load_extension(f"cogs.{files[:-3]}")
 
+# for files in os.listdir("./cogs"):
+#     if files.endswith(".py"):
+#         bot.load_extension(f"cogs.{files[:-3]}")
+bot.load_extension(f"cogs.youtube")
 
 bot.run(token)
